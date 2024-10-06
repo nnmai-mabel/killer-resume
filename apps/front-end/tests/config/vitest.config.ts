a702516f67957ee4f61d14aca2703c97ce38903a
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-10-06 10:53:28 +1030

    e2e testing passed

--- apps/front-end/tests/config/vitest.config.ts
+++ apps/front-end/tests/config/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { UserConfig } from 'vitest/config'; // Import Vitest types
 
